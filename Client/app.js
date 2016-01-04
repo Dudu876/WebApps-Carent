6ff90f8a80e75587094423c0f4fed3c1cc693bfb
Michaelzvu <michael.zv@gmail.com>
2016-01-05 00:37:00 +0200

    change car manager

--- Client/app.js
+++ Client/app.js
@@ -1,4 +1,4 @@
 /**
  * Created by Dudu on 30/12/2015.
  */
-var carentApp = angular.module('carentApp', ['ngRoute', 'appRoutes']);
+var carentApp = angular.module('carentApp', ['ngRoute', 'appRoutes','ui.bootstrap']);
