faa370e8cc54a60e33335b8829eebb34ae8f5be2
Dudu <dudu876@gmail.com>
2016-01-05 23:58:19 +0200

    Orders module

---
+++ Client/factories/orderFactory.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Dudu on 05/01/2016.
+ */
+carentApp.factory('OrderService', ['$http', function($http) {
+
+    return {
+        // call to get all cars
+        get : function() {
+            return $http.get('/api/orders');
+        },
+
+
+        // these will work when more API routes are defined on the Node side of things
+        // call to POST and create a new car
+        create : function(orderDara) {
+            return $http.post('/api/orders', orderDara);
+        },
+
+        // call to DELETE a car
+        delete : function(id) {
+            return $http.delete('/api/orders/' + id);
+        }
+    }
+
+}]);
