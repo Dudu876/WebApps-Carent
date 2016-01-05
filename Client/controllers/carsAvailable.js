55e5f58d711b88a9f5ba714f86a29ff18d1df07a
Dudu <dudu876@gmail.com>
2016-01-05 18:56:15 +0200

    doing pull

--- Client/controllers/carsAvailable.js
+++ Client/controllers/carsAvailable.js
@@ -3,6 +3,20 @@
  */
 carentApp.controller('carsAvailable', function($scope) {
 
-    $scope.tagline = 'To the moon and back!';
+    $scope.cars = [{
+        number: 123,
+        type: { manufacturer: "פיג'ו", model: "107", year: 2011},
+        category: "A",
+        price: 125,
+        gearbox: "ידני",
+        entryDate: "09/09/2010"
+    },{
+        number: 124,
+        type: { manufacturer: "פיג'ו", model: "108", year: 2015},
+        category: "A",
+        price: 180,
+        gearbox: "ידני",
+        entryDate: "09/09/2015"
+    }]
 
 });
