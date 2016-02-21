b0cb2d4c93d59d12138b0fc530a8b32ee20c78f7
Dudu <dudu876@gmail.com>
2016-02-21 14:53:33 +0200

    fix car branch not updated

--- Server/controllers/Cars.js
+++ Server/controllers/Cars.js
@@ -56,7 +56,7 @@ exports.updateCar = function (req, res) {
             car.category = req.body.category;
             car.price = req.body.price;
             car.gearbox = req.body.gearbox;
-            car.branch = req.body.branch;
+            car.branch = req.body.branch._id;
 
             car.save(function (err) {
                 if (!err) {
