c9b3b120aa2e210b44e56fbe1575decb3006ddc2
Dudu <dudu876@gmail.com>
2016-02-29 00:19:14 +0200

    order manager add client data

--- Client/controllers/ordersManager.js
+++ Client/controllers/ordersManager.js
@@ -1,15 +1,16 @@
 /**
  * Created by Dudu on 18/01/2016.
  */
-carentApp.controller('ordersManager', ['$scope', 'OrderService', function ($scope, OrderService) {
+carentApp.controller('ordersManager', ['$scope', 'OrderService', 'branchService', function ($scope, OrderService, branchService) {
 
     var FORMAT = "DD/MM/YYYY HH:mm";
 
     OrderService.get().success(function (response) {
         $scope.orders = response;
         $scope.orders.forEach(function (element, index, array) {
-            element.startDate = moment(element.startDate).format(FORMAT)
-            element.endDate = moment(element.endDate).format(FORMAT)
+            element.startDate = moment(element.startDate).format(FORMAT);
+            element.endDate = moment(element.endDate).format(FORMAT);
+            element.car.branch = branchService.getBranchName(element.car.branch);
         });
     });
 
