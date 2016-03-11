90f7a79b3eb91ef3de92bed5861f74e1981310f0
Dudu876 <dudu876@gmail.com>
2016-03-11 17:13:36 +0200

    timepicker and refresh time every 30 min

--- Client/controllers/carsAvailable.js
+++ Client/controllers/carsAvailable.js
@@ -1,16 +1,21 @@
 /**
  * Created by Dudu on 30/12/2015.
  */
-carentApp.controller('carsAvailable', ['$scope', '$location', '$uibModal', 'OrderService', 'carFactory', 'branchService', function ($scope, $location, $uibModal, OrderService, carFactory, branchService) {
+carentApp.controller('carsAvailable', ['$scope', '$location', '$timeout', '$uibModal', 'OrderService', 'carFactory', 'branchService', function ($scope, $location, $timeout, $uibModal, OrderService, carFactory, branchService) {
 
     $scope.selectedDay = {};
-    //$scope.selectedDay.date = moment().add(2,'hours'); //@dudu
     $scope.selectedDay.date = moment();
+    $scope.selectedDay.date.set('minute',0);
     $scope.search = {};
     $scope.search.branch = {};
     $scope.categories = ["","A","B","C","D","E","F","G"];
     $scope.carReturning = [];
     $scope.events = [];
+
+    $scope.ismeridian = false;
+    $scope.time = {};
+    $scope.time.mstep = 60;
+    $scope.time.hstep = 1;
 
     carFactory.get().success(function (response) {
         $scope.allCars = response;
@@ -31,7 +36,6 @@ carentApp.controller('carsAvailable', ['$scope', '$location', '$uibModal', 'Orde
         $scope.freeCars = $scope.allCars;
         if ($scope.orders === undefined || $scope.orders.length == 0) {
             $scope.freeCars = $scope.allCars;
-
             return 0;
         }
 
@@ -44,12 +48,8 @@ carentApp.controller('carsAvailable', ['$scope', '$location', '$uibModal', 'Orde
                 orderedCar.mission = element.mission;
                 orderedCar.phone = element.phone;
                 orderedCar.client_name = element.client_name;
-                //var now = new Date();
                 var now = $scope.selectedDay.date;
                 var diffDays = 0;
-                //var diffHours = Math.round((Date.parse(element.endDate) - now.getTime()) / 3600000);
-                //if (now.isBefore(moment(element.endDate).add(2,'hours'))) {
-                //var diffHours = Math.round(moment(element.endDate).add(2, 'hours').diff(now, 'minutes') / 60);
                 var diffHours = Math.round(moment(element.endDate).diff(now, 'minutes') / 60);
                 orderedCar.hours = diffHours;
                 if (diffHours > 24) {
@@ -60,7 +60,6 @@ carentApp.controller('carsAvailable', ['$scope', '$location', '$uibModal', 'Orde
                 }
                 orderedCar.returning += (diffHours.toString() + ((diffHours == 1) ? ' hour' : ' hours'));
                 $scope.carReturning.push(orderedCar);
-                //}
             }
         });
     }
@@ -79,7 +78,7 @@ carentApp.controller('carsAvailable', ['$scope', '$location', '$uibModal', 'Orde
 
     function getActiveOrders(allOrders, date) {
         return allOrders.filter(function (order) {
-            return date.isBetween(order.startDate, order.endDate, 'minute');
+            return date.isBetween(order.startDate, order.endDate);
         });
     }
 
@@ -115,39 +114,42 @@ carentApp.controller('carsAvailable', ['$scope', '$location', '$uibModal', 'Orde
     function calendarEvents(){
         $scope.allOrders.forEach(function (element, index, array) {
             var event = {};
-            //event.date = moment(element.endDate).toISOString();
             event.date = moment(element.endDate).subtract(2,'hours').format(); //@dudu
-            //event.date = moment(element.endDate).format();
             event.mission = element.mission;
             var car = angular.copy(element.car);
             car.branch = branchService.getBranchName(element.car.branch);
             event.car = car;
-            //event.title = car.type.manufacturer + ' ' + car.type.model + ' - ' + car.type.year + ' (' + car.branch + ')';
             $scope.events.push(event);
         });
     }
 
-    $scope.showClient = function(car) {
-        alert(car.client_name + ' - ' + car.phone);
-    };
-
     $scope.showEvents = function(day) {
-        //alert(events.map(function(e) { return e.title }).join("\n"));
         $scope.dayEvents = day.events;
-        //$scope.selectedDay = angular.copy(day);
-        var tempday = angular.copy(day);
-        //$scope.selectedDay.date.add(2, 'hours'); // @dudu
-        //$scope.selectedDay.date = day.date.add(2,'hours');
-        $scope.selectedDay = tempday;
-        //$scope.selectedDay.date = tempday.date.add(2,'hours');
-        //if (day.date.get('date') == moment().get('date')) $scope.selectedDay.date = moment().add(2,'hours');
+        $scope.selectedDay = angular.copy(day);
+        $scope.selectedDay.date.set({'hour':12,'minute':0});
         if (day.date.get('date') == moment().get('date') && day.date.get('month') == moment().get('month')) {
             $scope.selectedDay.date = moment();
+            $scope.selectedDay.date.set('minute',0);
         }
-        //$scope.selectedDay.date.utcOffset('+0200');
         $scope.orders = getActiveOrders($scope.allOrders, $scope.selectedDay.date);
         organizeData();
     };
+
+    $scope.timeChanged = function() {
+        $scope.selectedDay.date = moment($scope.selectedDay.date);
+        console.log($scope.selectedDay.date.toString());
+        $scope.orders = getActiveOrders($scope.allOrders, $scope.selectedDay.date);
+        organizeData();
+    };
+
+    function refreshDates() {
+        $scope.selectedDay.date = moment();
+        $scope.selectedDay.date.set('minute',0);
+        $scope.selectedDay.day = moment().date();
+        $scope.timeChanged();
+        $timeout(refreshDates, (30 * 60000));
+    }
+    $timeout(refreshDates, (30 * 60000));
 
     $scope.openModal = function (size, car, date) {
 
